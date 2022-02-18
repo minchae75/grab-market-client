be5c77ea3904fe45d081786fdb982ee5cb277a21
min <2262924@gmail.com>
2022-02-18 16:26:12 +0900

    api url 주소변경_final

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export const API_URL =
-  process.env.NODE_ENV === "production "
+  process.env.NODE_ENV === "production"
     ? "https://min-market-server.herokuapp.com"
     : "http://localhost:8080";
